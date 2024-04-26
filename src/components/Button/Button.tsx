1bd2704ab5c612bb8fe0a3941c40d1a354c7a68b
Rohan Fulzele <34005260+Rohanfulzele@users.noreply.github.com>
2024-04-26 20:10:49 +0530

    font family fix

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,8 +13,8 @@ const Button: React.FC<ButtonProps> = ({ variant,rounded, ...props }) => {
     if(variant === 'primary') return { borderColor: '#6366F1', backgroundColor: '#6366F1', color: '#FAFAFA', ...propStyle };
     if(variant === 'secondary') return { borderColor: '#6366F1', backgroundColor: '#FAFAFA', color: '#6366F1', ...propStyle };
     if(variant === 'danger') return { borderColor: '#7F1D1D', backgroundColor: '#7F1D1D', color: '#FAFAFA', ...propStyle };
-    if(variant === 'outline') return { borderColor: '#FAFAFA', backgroundColor: '#09090b', color: '#FAFAFA', ...propStyle };
-    if(variant === 'link') return {textDecoration: 'underline black', borderColor: 'transparent', backgroundColor: 'transparent', color: '#FAFAFA', ...propStyle };
+    if(variant === 'outline') return { borderColor: '#FAFAFA', backgroundColor: 'transparent', color: '#FAFAFA', ...propStyle };
+    if(variant === 'link') return {textDecoration: 'underline black', borderColor: 'transparent', backgroundColor: 'transparent',border:'none', color: '#FAFAFA', ...propStyle };
     
     if(rounded) return { borderRadius: '100vw', ...propStyle }
 
